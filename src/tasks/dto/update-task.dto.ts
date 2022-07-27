54059b337bb469854f64123a246344d5d4417378
Desouky <alaa.desouky33@gmail.com>
2022-07-28 01:20:03 +0200

    Added class validation

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,6 +1,9 @@
+import { IsEnum } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class UpdateTaskDto {
   id: string;
+
+  @IsEnum(TaskStatus)
   status: TaskStatus
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid'
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id)
+    const foundTask = this.tasks.find(task => task.id === id)
+    if (!foundTask) {
+      throw new NotFoundException(`Task with ID: '${id}' was not found`)
+    }
+
+    return foundTask;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
